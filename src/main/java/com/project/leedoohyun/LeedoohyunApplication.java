37c471735fdb860705bf6e81baecdffd8813be21
doo <doohyun.lee@homeplus.co.kr>
2022-04-14 19:48:17 +0900

    project init

---
+++ src/main/java/com/project/leedoohyun/LeedoohyunApplication.java
@@ -0,0 +1,13 @@
+package com.project.leedoohyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeedoohyunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeedoohyunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/leedoohyun/LeedoohyunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.leedoohyun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeedoohyunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
